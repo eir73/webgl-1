3907fc3ea5249dd6f4a1fd242dcf50dcaba64f0a
eir73 <roflaneir73@gmail.com>
2020-09-06 22:36:22 +0300

    enable to control rabbit with kb arrows

--- main.js
+++ main.js
@@ -18,16 +18,16 @@ const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
 const positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer) 
 
-const positions = [
+let positions = [
   60, 270,
   120, 270,
   120, 210,
   120, 210,
   60, 210,
-  60, 270, //head end
+  60, 270, //head 
   120, 270,
   180, 270,
-  180, 330, //ears end
+  180, 330, //ears 
   120, 240,
   120, 120,
   240, 120, //body tr1
@@ -45,29 +45,31 @@ const positions = [
   60, 90,
   60, 90, 
   90, 120,
-  180, 120
-
+  180, 120 //hands
 ];
+
+positions = verticalMove(1, positions)
+
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
 gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-gl.clearColor(0, 0, 0, 0)
+gl.clearColor(1, 0.7, 0.8, 1)
 gl.clear(gl.COLOR_BUFFER_BIT)
 gl.useProgram(program)
 gl.enableVertexAttribArray(positionAttributeLocation)
-gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
 // Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
-const size = 2;          // 2 компоненты на итерацию
-const type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
-const normalize = false; // не нормализовать данные
-const stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
-const offset = 0;        // начинать с начала буфера
+const size = 2          // 2 компоненты на итерацию
+const type = gl.FLOAT   // наши данные - 32-битные числа с плавающей точкой
+const normalize = false // не нормализовать данные
+const stride = 0        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
+const offset = 0       // начинать с начала буфера
 gl.vertexAttribPointer(
     positionAttributeLocation, size, type, normalize, stride, offset)
 
-const primitiveType = gl.TRIANGLES;
-const count = positions.length;
-gl.drawArrays(primitiveType, offset, count);
+const primitiveType = gl.TRIANGLES
+const count = positions.length
+gl.drawArrays(primitiveType, offset, count)
 
 function createShader(gl, type, source) {
   const shader = gl.createShader(type)  // создание шейдера
@@ -94,4 +96,47 @@ function createProgram(gl, vertexShader, fragmentShader) {
 
   console.log(gl.getProgramInfoLog(program))
   gl.deleteProgram(program)
+}
+
+function verticalMove(dir, buffer) {
+  return buffer.map((el, i) => {
+    if(i % 2) {
+      return el + dir * 30
+    } 
+    return el
+  })
+}
+
+function horizontalMove(dir, buffer) {
+  return buffer.map((el, i) => {
+    if(i % 2) {
+      return el
+    } 
+    return el + dir * 30
+  })
+}
+
+document.onkeydown = handleButtonClick
+
+function handleButtonClick(e) {
+  switch (e.key) {
+    case 'ArrowUp': 
+      positions = verticalMove(1, positions)
+      break
+    case 'ArrowDown':
+      positions = verticalMove(-1, positions)
+      break
+    case 'ArrowLeft':
+      positions = horizontalMove(-1, positions)
+      break
+    case 'ArrowRight':
+      positions = horizontalMove(1, positions)
+      break
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+  gl.clearColor(1, 0.7, 0.8, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  const primitiveType = gl.TRIANGLES
+  const count = positions.length
+  gl.drawArrays(primitiveType, offset, count)
 }
